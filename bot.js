94231be88c679d35497488ebdbcb0a09d848f6b2
A. L. H <audelise.h@gmail.com>
2017-07-01 20:44:44 +0200

    fixed keys

--- bot.js
+++ bot.js
@@ -8,10 +8,10 @@ const comparisons = require('./strings/comparisons.js');
 
 //make a new twit object with config
 const T = new Twit({
-  consumer_key:         CONSUMER_KEY,
-  consumer_secret:      CONSUMER_SECRET,
-  access_token:         ACCESS_TOKEN,
-  access_token_secret:  ACCESS_TOKEN_SECRET, 
+  consumer_key:         process.env.process.env.process.env.process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.process.env.process.env.CONSUMER_SECRET,
+  access_token:         process.env.process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET, 
 });
 
 //----------------------------
